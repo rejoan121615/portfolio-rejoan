9ff89f2ccf7d4d7f371dcefdf3110f051eeed560
-h <wearecrx@gmail.com>
2022-11-10 10:38:08 +0600

    menu section complete

--- js/script.js
+++ js/script.js
@@ -1,17 +1,54 @@
+// gsap animation
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const menuSplitText = new SplitText("#menu li a", { type: "lines" });
+
 // menu function
 const navBtn = document.querySelector(".nav-btn button");
-const menu = document.querySelector("#menu");
+// const menu = document.querySelector("#menu");
 let navCounter = true;
+
+const menuTL = gsap.timeline({ paused: true });
+
+// menuTL.fromTo(
+//     "#menu",
+//     {
+//         clipPath: "circle(0% at calc((100vw - 5%)) 45px)",
+//     },
+//     {
+//         clipPath: "circle(150% at 90% 28px)",
+//         duration: 0.8,
+//         delay: 0.15,
+//     }
+// );
+menuTL
+    .fromTo(
+        menuSplitText.lines,
+        {
+            y: "115%",
+        },
+        {
+            y: "0%",
+            duration: 0.6,
+        }
+    )
+
 function MenuHandler(target) {
     console.log(target);
     if (navCounter) {
         navBtn.classList.add("opened");
-        menu.classList.add("open");
         navBtn.setAttribute("aria-expanded", navCounter);
+        menu.classList.add("open");
+        setTimeout(() => {
+            menuTL.play();
+        }, 800);
     } else {
-      navBtn.classList.remove("opened");
-      menu.classList.remove("open");
-      navBtn.setAttribute("aria-expanded", navCounter);
+        setTimeout(() => {
+            navBtn.classList.remove("opened");
+            navBtn.setAttribute("aria-expanded", navCounter);
+            menu.classList.remove("open");
+        }, 800);
+        menuTL.reverse();
     }
     navCounter = !navCounter;
 }
