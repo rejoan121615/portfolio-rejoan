74407394d9f8939e9d896425fc19804dccf85ac2
-h <wearecrx@gmail.com>
2022-11-10 07:06:11 +0600

    menu animation created

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
-
-// menu function 
+// menu function
 const navBtn = document.querySelector(".nav-btn button");
-
-let navCounter = false;
-function MenuHandler() {
-  if (navCounter) {
-    
-  } else {
-  }
-  navCounter = !navCounter;
-  console.log(navCounter);
+const menu = document.querySelector("#menu");
+let navCounter = true;
+function MenuHandler(target) {
+    console.log(target);
+    if (navCounter) {
+        navBtn.classList.add("opened");
+        menu.classList.add("open");
+        navBtn.setAttribute("aria-expanded", navCounter);
+    } else {
+      navBtn.classList.remove("opened");
+      menu.classList.remove("open");
+      navBtn.setAttribute("aria-expanded", navCounter);
+    }
+    navCounter = !navCounter;
 }
-
 navBtn.onclick = MenuHandler;
