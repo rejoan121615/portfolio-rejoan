b6095a6f610fab8e11b9441226dec9aedd506d13
-h <wearecrx@gmail.com>
2022-11-11 12:36:22 +0600

    loading animation complete

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
+console.clear();
 // gsap animation
-gsap.registerPlugin(ScrollTrigger, SplitText);
+gsap.registerPlugin(ScrollTrigger, SplitText, CSSRulePlugin);
 
 const menuSplitText = new SplitText("#menu li a", { type: "lines" });
-
 
 // menu function
 const navBtn = document.querySelector(".nav-btn button");
@@ -22,17 +22,16 @@ const menuTL = gsap.timeline({ paused: true });
 //         delay: 0.15,
 //     }
 // );
-menuTL
-    .fromTo(
-        menuSplitText.lines,
-        {
-            y: "115%",
-        },
-        {
-            y: "0%",
-            duration: 0.6,
-        }
-    )
+menuTL.fromTo(
+    menuSplitText.lines,
+    {
+        y: "115%",
+    },
+    {
+        y: "0%",
+        duration: 0.6,
+    }
+);
 
 function MenuHandler(target) {
     console.log(target);
@@ -55,24 +54,95 @@ function MenuHandler(target) {
 }
 navBtn.onclick = MenuHandler;
 
-
 // main section animation
-new SplitText('#main .name-container h1', {
-  type: 'chars',
-  charsClass: 'letter-wrap',
-})
-
-const mainTitle = new SplitText('#main .name-container h1', {
-  type: 'chars',
-  charsClass: 'letter',
-})
-
-console.log(mainTitle);
-
-gsap.fromTo(mainTitle.chars, {
-  x: "-100%"
-}, {
-  x: "0",
-  duration: 0.8,
-  delay: 1,
+new SplitText("#main .name-container h1", {
+    type: "chars",
+    charsClass: "letter-wrap",
 });
+
+const mainTitle = new SplitText("#main .name-container h1", {
+    type: "chars",
+    charsClass: "letter",
+});
+
+// console.log(mainTitle);
+
+// gsap.fromTo(mainTitle.chars, {
+//   x: "-100%"
+// }, {
+//   x: "0",
+//   duration: 0.8,
+//   delay: 1,
+// });
+
+// loading indicator
+const loaderCircle = document.querySelectorAll("#main #loader circle");
+const loaderBg = document.querySelector("#loading-bg");
+
+const loadingTl = gsap.timeline();
+
+const completeDash = "910px 1000px";
+
+loadingTl
+    .set("#main #loader", {
+        y: '50%'
+    })
+    .set(loaderCircle, {
+        strokeDasharray: "0px 1000px",
+    })
+    .to(loaderCircle[0], {
+        strokeDasharray: completeDash,
+        duration: 1,
+        delay: 1,
+    })
+    .to(loaderCircle[1], {
+        strokeDasharray: completeDash,
+        duration: 1,
+    })
+    .to(
+        loaderCircle[2],
+        {
+            strokeDasharray: completeDash,
+            duration: 0.8,
+            delay: 0.2,
+        },
+        "<"
+    )
+    .to("#loading-bg .shade", {
+        scaleY: "0",
+        duration: 1.4,
+        delay: 0.5,
+        stagger: {
+            from: "end",
+            each: 0.05,
+        },
+        onComplete: () => {
+            gsap.set("#main #loader", {
+                zIndex: 0,
+            });
+            // document.querySelector("#loading-bg").remove();
+        },
+    });
+// .to(
+//     CSSRulePlugin.getRule("#loading-bg:before"),
+
+//     {
+//         cssRule: {
+//             scaleY: "0",
+//         },
+//         duration: 0.75,
+//         ease: "power1.out",
+//         delay: 0.1,
+//     },
+//     "<"
+// )
+//     .to(
+//         "#loading-bg",
+//         {
+//             scaleY: "0",
+//             duration: 0.75,
+//             ease: "power1.out",
+//             delay: 0.1,
+//         },
+//         "<"
+//     );
